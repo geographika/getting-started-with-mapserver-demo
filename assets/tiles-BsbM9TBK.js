4de09af1eb61098149ab048bd52e0d8eed17e6e0
geographika <geographika@users.noreply.github.com>
2024-05-05 14:34:53 +0000

    deploy: geographika/getting-started-with-mapserver@7d0a3c7400d533f1276c3b04d59f4a61157a4097

---
+++ assets/tiles-BsbM9TBK.js
@@ -0,0 +1,2 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";import{M as e,V as r}from"./Layer-Dn2Bair9.js";/* empty css              */import{T as a,X as s}from"./Tile-Bu-483El.js";import"./TileLayer-DI1GLsdq.js";const t="https://api.mapserverstudio.net/mapserver/?",p="map=/etc/mapserver/mapfiles/workshop/";new e({target:"map",layers:[new a({source:new s({url:t+p+"tiles.map&MODE=tile&TILE={x}+{y}+{z}&LAYERS=countries"})})],view:new r({center:[-472202,7530279],zoom:5})});
+//# sourceMappingURL=tiles-BsbM9TBK.js.map
